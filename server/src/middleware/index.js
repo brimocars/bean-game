e300a9d015a81e3a4de36196e815e6bf677f48a4
Brian Mocarski <bsm1875@rit.edu>
2025-04-16 12:50:36 -0400

    fixing

--- server/src/middleware/index.js
+++ server/src/middleware/index.js
@@ -4,6 +4,7 @@ const auth = (req, res, next) => {
   const token = req.headers?.authorization?.split(' ')[1];
   if (!token) {
     // return res.status(401).send({ message: 'No token provided' });
+    console.log('redirect')
     return res.redirect('/login-page');
   }
 
